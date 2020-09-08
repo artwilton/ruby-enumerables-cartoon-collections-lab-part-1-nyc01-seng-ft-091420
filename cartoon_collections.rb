122c5b9d60f9a179dbaeb0cd8fdd11975d4b8f8f
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-08 19:42:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def greet_characters(array)
 end
 
 def list_dwarves(array)
-  array.each do |dwarves|
+  array.each_with_index do |dwarves|
     puts dwarves
   end
   # Use `each_with_index` to enumerate over the provided array
