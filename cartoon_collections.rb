18c1d1af8a8c7ac1be1fe1984152d20e0460b00a
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-08 19:45:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,7 +9,7 @@ end
 
 def list_dwarves(array)
   array.each_with_index do |dwarves, index|
-    puts dwarves
+    puts "#{index + 1}. #{dwarves}"
   end
   # Use `each_with_index` to enumerate over the provided array
   #
